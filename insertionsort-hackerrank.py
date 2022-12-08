383029497ebd1605611abbf5b6f3c803e3dd4819
nocturnal <106307629+abrambelay@users.noreply.github.com>
2022-12-08 21:15:42 +0300

    Create insertionsort-hackerrank.py

---
+++ insertionsort-hackerrank.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'insertionSort1' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY arr
+#
+
+def insertionSort1(n, arr):
+    key = arr[n-1]
+    i=n-2
+    def printarr():
+        for j in range(n):
+            print(arr[j],end=' ')
+    while i >= 0 and key<arr[i]:
+        arr[i+1]=arr[i]
+        printarr()
+        i=i-1
+        print('')
+    arr[i+1]=key
+    printarr()
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    insertionSort1(n, arr)
