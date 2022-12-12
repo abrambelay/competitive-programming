6a3a126dc6b5c21041dc778de4bb347eeaa94020
nocturnal <106307629+abrambelay@users.noreply.github.com>
2022-12-12 17:03:42 +0300

    Create queueusingstacks.py

---
+++ queueusingstacks.py
@@ -0,0 +1,35 @@
+class MyQueue(object):
+
+    def __init__(self):
+        self.stack= []
+
+    def push(self, x):
+        self.stack.append(x)
+        
+
+    def pop(self):
+        queue = []
+        if len(self.stack)!=0:
+            while len(self.stack)>0:
+                queue.append(self.stack.pop())
+            res =  queue.pop()
+            while len(queue)>0:
+                self.stack.append(queue.pop())
+            return res
+        
+
+    def peek(self):
+        return self.stack[0]
+        
+
+    def empty(self):
+        return len(self.stack)==0
+        
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
