00342da1237564be1cbb2b9bbbaab1f027de1d4c
nocturnal <106307629+abrambelay@users.noreply.github.com>
2022-12-30 04:49:37 +0300

    Update fibonnacinumber-leetcode.py

--- fibonnacinumber-leetcode.py
+++ fibonnacinumber-leetcode.py
@@ -1,9 +1,7 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n==0:
-            return 0
-        if n==1:
-            return 1
+        if n<=1:
+            return n
         prev=0
         next=1
         for i in range(n-1):
