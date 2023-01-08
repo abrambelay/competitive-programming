dc67b94f3934b35fe39160fb793cc56099013f7d
nocturnal <106307629+abrambelay@users.noreply.github.com>
2023-01-08 11:06:21 +0300

    Create findthewinnerofcirculargame-leetcode.py

---
+++ findthewinnerofcirculargame-leetcode.py
@@ -0,0 +1,27 @@
+#recursive solution
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        lst = []
+        for i in range(1,n+1):
+            lst.append(i)
+        start = 0 
+        def FindWinner(ind):
+            if len(lst)==1:
+                return lst[0]
+            else:
+                lst.pop(ind)
+                start = ind%len(lst)
+                return FindWinner((start + k-1)%len(lst))
+        return FindWinner((k-1)%len(lst))
+#itrative solution
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        lst = []
+        for i in range(1,n+1):
+            lst.append(i)
+        start = 0
+        while len(lst)>1:
+            ind = (start + k-1)%len(lst)
+            lst.pop(ind)
+            start = ind%len(lst)
+        return lst[0]
