dfc5824f61163431658d39f09aca009da8154d0f
nocturnal <106307629+abrambelay@users.noreply.github.com>
2023-01-21 19:24:40 +0300

    Update nextgreaterelement-leetcode.py

--- nextgreaterelement-leetcode.py
+++ nextgreaterelement-leetcode.py
@@ -1,13 +1,14 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        arr = []
-        for i in range(len(nums1)):
-            for j in range(len(nums2)):
-                if nums1[i]==nums2[j]:
-                    for k in range(j+1,len(nums2)):
-                        if nums2[k]>nums2[j]:
-                            arr.append(nums2[k])
-                            break
-                    if len(arr)<i+1:
-                        arr.append(-1)
-        return arr
+        ans = []
+        stack = []
+        hashmap = {}
+        for i in range(len(nums2)-1,-1,-1):
+            while stack and nums2[i] >= stack[-1]:
+                stack.pop()
+            if stack:
+                hashmap[nums2[i]]=stack[-1]
+            stack.append(nums2[i])
+        for indx,num in enumerate(nums1):
+            ans.append(hashmap.get(num,-1))
+        return ans
